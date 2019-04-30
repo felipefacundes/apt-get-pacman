6103fb994a323fb94501173318ddf02432e26032
felipefacundes <felipe.facundes@gmail.com>
2019-04-30 01:39:32 -0300

    Para aqueles que tem dificuldade de usar o pacman, e já estão acostumados com o apt-get.
    Para aqueles que tem dificuldade de usar o pacman, e já estão acostumados com o apt-get.

--- iniciorapido.sh
+++ iniciorapido.sh
@@ -10,7 +10,7 @@ function log_error {
     echo "$(tput setaf 1)ERROR: $1$(tput sgr0)" >&2
 }
 
-# Try to detect if we are root
+# Detectar se é root
 if [ "$(whoami)" != "root" ]; then
     use_sudo=true
 fi
@@ -22,7 +22,7 @@ function do_sudo {
     fi
 }
 
-# Match any [options]
+# Opções
 while :
 do
     case "$1" in
@@ -30,32 +30,32 @@ do
             noconfirm=true
             shift
             ;;
-        --) # End of all options
+        --) # Fim das opções
             shift
             break
             ;;
         -*)
-            log_error "Unknown option: $1"
+            log_error "Opção vazia: $1"
             break
             ;;
-        *)  # No more options
+        *)  # Sem mais opções
             break
             ;;
     esac
 done
 
-log_info "Cloning pac repo to /tmp/pac"
+log_info "Clonando apt-get-pacman para /tmp/apt-get-pacman"
 git clone https://github.com/felipefacundes/apt-get-pacman.git /tmp/apt-get-pacman
 
-log_info "Installing pac to /usr/bin/"
+log_info "Instalando apt-get-pacman para /usr/bin/"
 do_sudo install /tmp/apt-get-pacman/bin/apt-get /usr/bin/apt-get
 
 if [ "$noconfirm" = true ]; then
     install_yay=true
 else
-    read -p "Use yay (y/n)?" -n 1 choice; echo
+    read -p "Vai usar o yay (s/n)?" -n 1 choice; echo
     case "$choice" in
-        y|Y)
+        s|Y)
             install_yay=true
             ;;
         n|N )
@@ -68,9 +68,9 @@ else
     esac
 fi
 if [ "$install_yay" = true ]; then
-    log_info "Creating default ~/.yayrc"
+    log_info "Criando configuração ~/.yayrc"
     echo "BUILD_NOCONFIRM=1\nEDITFILES=0" > ~/.yayrc
-    log_info "Installing yay"
+    log_info "Instalando yay"
     do_sudo apt-get update --noconfirm
     do_sudo apt-get install yay --noconfirm
 fi
